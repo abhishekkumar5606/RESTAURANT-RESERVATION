16d527b76df07a55159131c0056a5d0d0d55ed5e
Abhira Bhai <vasuraj5606@gmail.com>
2024-03-15 14:18:05 +0530

    cors updated url

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://restaurant-reservation-blue.vercel.app/",
     methods: ["POST"],
     credentials: true,
   })
